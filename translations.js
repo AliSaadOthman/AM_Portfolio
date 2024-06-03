a58f617057f0219d1eefcf76a2b218387ec23648
AliSaadOthman <alisaadothman21@gmail.com>
2024-06-03 17:47:51 +0300

    Change into portfolio

---
+++ translations.js
@@ -0,0 +1,20 @@
+const translations = {
+  en: {
+    navHome: "Home",
+    navAbout: "About",
+    navProjects: "Projects",
+    navContact: "Contact",
+
+    CopyrightText: "© 2024 Prototype Website Co. All rights reserved.",
+    SocialMediaCTA: "Follow us on Social Media:",
+  },
+  ar: {
+    navHome: "الرئيسيه",
+    navAbout: "عن الشركة",
+    navProjects: "المشاريع",
+    navContact: "التواصل",
+
+    CopyrightText: "© 2024 شركة موقع بروتوتايب. جميع الحقوق محفوظة.",
+    SocialMediaCTA: "تابعنا على وسائل التواصل الإجتماعي:",
+  },
+};
