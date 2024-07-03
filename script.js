298ffbc4d61bde35caeeccaace823670af019c0d
AliSaadOthman <alisaadothman21@gmail.com>
2024-07-03 17:58:05 +0300

    fixed footer and navbar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 $(document).ready(function () {
   // Load the navbar
-  $("#navbar").load("../Components/navbar.html");
+  $("#navbar").load("Components/navbar.html");
   // Load the footer
-  $("#footer").load("../Components/footer.html");
+  $("#footer").load("Components/footer.html");
 });
